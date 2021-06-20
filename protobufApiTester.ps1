f10cebcb1bdadb54e3d0674e7369d9c1cc01e800
rorozco1997 <44987991+rorozco1997@users.noreply.github.com>
2021-06-19 21:46:58 -0700

    Fixing bugs

--- protobufApiTester.ps1
+++ protobufApiTester.ps1
@@ -7,6 +7,6 @@ Param(
 
 npm install
 
-npm run pbjs -- -t json-module -w commonjs -o "$PSScriptRoot/src/protobuf/bundle.js" $pathsToProto
+npm run pbjs -- --path $protoRoot -t json-module --es6 -w commonjs -o "$PSScriptRoot/src/protobuf/bundle.js" $pathsToProto
 
 npm start
